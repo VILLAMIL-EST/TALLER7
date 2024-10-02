ae8353ceb561e0399782ea10bcd5afb8a844219e
Andre ruiz <adavidruiz@ucundinamarca.edu.co>
2024-10-02 10:53:34 -0500

    Ruta estudiantes

---
+++ backend/routes/estudiantesroutes.js
@@ -0,0 +1,14 @@
+/*const express:e|{}=>core.Express = require("express");
+const router : Router =express.Router();
+const estudiantescontroller:{}= require("../controllers/estudiantescontrollers.js");
+
+router.get(path:"/",estudiantescontroller.consultar);
+router.get(path "/",estudiantescontroller.ingresar);
+
+router.route(prefix: "/:id")
+.get(estudiantescontroller.consultarDetalle)
+  .put(estudiantescontroller.actualizar)
+  .delete(estudiantescontroller.borrar);
+
+module.exports = router;
+*/
