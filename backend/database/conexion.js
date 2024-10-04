f8040a04015bd79211fe775b1b56df38c6a43d43
yecid vilamil <estivenvillamilballen@gmail.com>
2024-10-04 01:52:33 -0500

    conexion

---
+++ backend/database/conexion.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+    host: 'localhost',          
+    user: 'root',
+    password: 'Monkey1076648517',
+    database: 'cursos'
+});
+
+// Establecer la conexión
+conexion.connect((err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos: ', err);
+        return;
+    }
+    console.log('Conexión establecida con éxito');
+});
