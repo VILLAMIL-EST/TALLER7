e7f8ac3cde2d773af27bed4122e49fbc33db63fc
Yecid <villamilballenestiven@gmail.com>
2024-10-09 09:20:03 -0500

    vamos bien

--- netlify/funciones/estudiantes.js
+++
@@ -1,9 +0,0 @@
-const estudiantesRouter = require('../../backend/router/estudiantesrouter');
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-app.use(express.json());
-app.use('/.netlify/functions/estudiantes', estudiantesRouter);
-
-module.exports.handler = serverless(app);
